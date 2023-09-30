c1855275cac27f566ce52b15c20d14715dc59561
Lucas Cruz <lucascruz.jsdeveloper@gmail.com>
2023-09-29 22:45:06 -0300

    add database sample

--- src/main/api/index.ts
+++ src/main/api/index.ts
@@ -6,12 +6,32 @@
 // console.log(hello.say())
 
 import express from 'express';
+import { Pool, Client } from 'pg';
+const connectionString = 'postgres://admin:MWC9yfmcyyolZxy5xbLvBywLUTu7NoK9@dpg-ckbn9iect0pc73fbro00-a.oregon-postgres.render.com/dd_digital_helper_db?ssl=true';
+
+
+const client = new Client({
+  connectionString,
+});
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, response) => {
+  client.connect();
+
+  // client.query(`INSERT into example (texto) values('Qualquer coisa');`, (err, res) => {
+  client.query(`SELECT * FROM example;`, (err, res) => {
+    if (!err) {
+      console.log(res.rows);
+      response.send(`esse texto veio do banco de dados: ${res.rows[1].texto}`);
+    } else {
+      console.log(err.message);
+      response.send(`algum erro aconteceu:\n${err.message}`);
+    }
+
+    client.end;
+  });
 })
 
 app.listen(port, () => {
